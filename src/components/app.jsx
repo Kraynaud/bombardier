228a901eaeb568d5bf90d0b095014cf09151c6f9
kevinraynaud <kraynaud@gmail.com>
2020-09-15 19:19:02 +0200

    data check rectified

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -33,38 +33,46 @@ class App extends Component {
     calculateData = (data) => {
         
         let array;
-        if (this.state.deviationIsa === -20) {
+        if (this.state.deviationIsa <= -16) {
             array = isaM20;
-        } else if (this.state.deviationIsa === -10) {
+        } else if (this.state.deviationIsa >= -15 && this.state.deviationIsa <= -6) {
             array = isaM10;
-        } else if (this.state.deviationIsa === 0) {
+        } else if (this.state.deviationIsa >= -5 && this.state.deviationIsa <= 4) {
             array = isa;
-        } else if (this.state.deviationIsa === 10) {
+        } else if (this.state.deviationIsa >= 5 && this.state.deviationIsa <= 14) {
             array = isa10;
-        } else if (this.state.deviationIsa === 20) {
+        } else if (this.state.deviationIsa >= 15 && this.state.deviationIsa <= 24) {
             array = isa20;
-        } else if (this.state.deviationIsa === 30) {
+        } else if (this.state.deviationIsa >= 25 && this.state.deviationIsa <= 32) {
             array = isa30;
-        } else if (this.state.deviationIsa === 35) {
+        } else if (this.state.deviationIsa >= 33 && this.state.deviationIsa <= 36) {
             array = isa35;
-        } else if (this.state.deviationIsa === 37) {
+        } else if (this.state.deviationIsa >= 37) {
             array = isa37;
         } else {
-            alert("Data out of indicated charts");
+            array = null;
         }
 
-        const filter = Object.values(array);
-        const result = [];
-        filter.forEach((item) => {
-            if ( item['poids'] == data['poidsAct'] && item['alt'] == data['altitudeAct'] ) {
-                result.push(item)   
-            }
-        });
-        this.setState({
-            ias : result[0]['ias'],
-            ff : result[0]['ff'],
-            trq : result[0]['trq'],
-        })
+        if (array !== null) {
+            const filter = Object.values(array);
+            const result = [];
+            filter.forEach((item) => {
+                if ( item['poids'] == data['poidsAct'] && item['alt'] == data['altitudeAct'] ) {
+                    result.push(item)   
+                }
+            });
+            this.setState({
+                ias : result[0]['ias'],
+                ff : result[0]['ff'],
+                trq : result[0]['trq'],
+            })
+        } else {
+            this.setState({
+                ias : "Out of indicated charts",
+                ff : "Out of indicated charts",
+                trq : "Out of indicated charts",
+            })
+        }
     }
 
     calculateMCP = (data) => {
@@ -73,7 +81,7 @@ class App extends Component {
         const altitude = parseInt(data['altitudeAct']);
         const temperature = parseInt(data['tempAct']);
         const altitudeTarget = parseInt(data['altTarget']);
-        const tempRound = Math.round(temperature/10) * 10;
+        const tempRound = temperature > 0 ? Math.floor((temperature+5)/10 ) * 10  : Math.floor((temperature-5)/10 ) * 10 ;    
         const mcpRecord = [];
 
         this.state.mcp.forEach((item) => {
@@ -86,6 +94,9 @@ class App extends Component {
             if (parseInt(key) === tempRound) {
                 mcpValue = mcpRecord[0][key]
             }
+        }
+        if (mcpValue === undefined) {
+            mcpValue = "MCP out of indicated charts"
         }
 
         tempMcp = ((altitudeTarget - altitude) / 1000 * -2) + temperature;
@@ -105,15 +116,15 @@ class App extends Component {
                     calculateMCP={this.calculateMCP}
                 />
                 <div className="results">
-                    <h2>Your ISA is {this.state.deviationIsa}</h2> 
+                    <h2>Your ISA deviation is {this.state.deviationIsa} (%)</h2> 
                     <h2>Your flight data</h2>
                         <ul>
-                            <li>Torque : {this.state.trq}%</li>
-                            <li>Indicated Air Speed : {this.state.ias}Kt</li>
-                            <li>Fuel Flow : {this.state.ff}Kg</li>
+                            <li>Torque : {this.state.trq} (%)</li>
+                            <li>Indicated Air Speed : {this.state.ias} (Kt)</li>
+                            <li>Fuel Flow : {this.state.ff} (Kg)</li>
                         </ul>
-                    <h2>Your Max Continuous Power is {this.state.mcpValue}%</h2>
-                    <h2>MCP temperature is {this.state.tempMcp}°C</h2>    
+                    <h2>Your Max Continuous Power is {this.state.mcpValue} (%)</h2>
+                    <h2>MCP temperature is {this.state.tempMcp} (°C)</h2>    
                 </div>
             </div>
         )
